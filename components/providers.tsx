05c62f6ccd8b4db95c8fa09f22b5ce0c449bf598
Mukesh Kr. Gupta <rg37090@gmail.com>
2024-07-21 13:24:08 +0530

    Train Page designed

---
+++ components/providers.tsx
@@ -0,0 +1,12 @@
+
+'use client'
+
+import {NextUIProvider} from '@nextui-org/react'
+
+export function UiProviders({children}: { children: React.ReactNode }) {
+  return (
+    <NextUIProvider>
+      {children}
+    </NextUIProvider>
+  )
+}
