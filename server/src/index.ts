75bc1e74181336096e288219abd4c9fe3885a787
Mukesh Kr. Gupta <rg37090@gmail.com>
2024-07-24 22:19:44 +0530

    Setting up postgres server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,19 @@
 import express, { Request, Response } from 'express';
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { PrismaClient } from '@prisma/client'
 import bodyParser from 'body-parser';
 import dotenv from "dotenv";
 import path from "path";
 dotenv.config({ path: "./.env" });
 import router from "./routes/flight"
-async function init() {
 
+
+const prisma = new PrismaClient()
+async function init() {
     const PORT: string | undefined = process.env.PORT;
 
-    if ( !PORT) {
+    if (!PORT) {
         console.error("Environment variables and PORT must be provided.");
         return;
     }
@@ -21,11 +24,19 @@ async function init() {
     app.use(express.json());
     app.use(express.urlencoded({ extended: false }));
     app.use(bodyParser.json());
-    app.use("/",router)
+    app.use("/", router)
 
     app.listen(PORT, () => {
         console.log(`server is running on http://localhost:${PORT}`);
     });
 }
 
-init();
+init()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
