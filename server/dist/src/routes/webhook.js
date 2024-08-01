82a2858e8dca1bba63c639a97c5e0f47771d845c
Sudip Kundu <sudip2003kundu@gmail.com>
2024-08-01 16:01:27 +0530

    start command changed

--- server/dist/src/routes/webhook.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var webHookRouter = (0, express_1.Router)();
-var webhook_1 = require("../controllers/webhook");
-webHookRouter.post("/getTrains", webhook_1.findTrains);
-exports.default = webHookRouter;
